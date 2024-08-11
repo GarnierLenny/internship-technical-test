98b55ba91bac460605ab4c7c5809c6834f5285b6
GarnierLenny <61317098+GarnierLenny@users.noreply.github.com>
2024-08-11 23:41:13 +0400

    FEAT - added color hex value part-2

--- part-2/src/App.tsx
+++ part-2/src/App.tsx
@@ -2,23 +2,36 @@ import React from 'react';
 import { styles } from './App.styles';
 
 function App() {
-  const baseColors: string[] = [
-    'rgb(255, 0, 0)',
-    'rgb(255, 165, 0)',
-    'rgb(255, 255, 0)',
-    'rgb(0, 128, 0)',
-    'rgb(0, 0, 255)',
+  const baseColors: number[][] = [
+    [255, 0, 0],
+    [255, 165, 0],
+    [255, 255, 0],
+    [0, 128, 0],
+    [0, 0, 255],
   ];
+
+
+  const componentToHex = (c: number) => {
+    var hex = c.toString(16);
+    return hex.length === 1 ? "0" + hex : hex;
+  }
+  const rgbToHex = (r: number, g: number, b: number) => {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+  }
 
   return (
     <div style={{display: 'flex'}}>
-      {baseColors.map((bgColor, index) => (
-        <div style={{...styles.section, backgroundColor: bgColor}} key={index}>
-          <div style={styles.textContainer}>
-            <p>{bgColor}</p>
+      {baseColors.map((bgColor, index) => {
+        const color = `rgb(${bgColor[0]}, ${bgColor[1]}, ${bgColor[2]})`;
+
+        return (
+        <div style={{...styles.section, backgroundColor: color}} key={index}>
+          <div style={{...styles.textContainer, flexDirection: "column"}}>
+            <p>{color}</p>
+            <p>{rgbToHex(bgColor[0], bgColor[1], bgColor[2])}</p>
           </div>
         </div>
-      ))}
+      )})}
     </div>
   );
 }
