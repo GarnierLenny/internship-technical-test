e17602dd35b563c274f3cb447d7b009cbdfe7aad
GarnierLenny <61317098+GarnierLenny@users.noreply.github.com>
2024-08-11 15:11:36 +0400

    FEAT - page now shows palette and generate palette button

--- part-1/static/script.js
+++ part-1/static/script.js
@@ -1,3 +1,13 @@
-function sayHello() {
-  console.log('Hello!');
+async function getNewPalette() {
+  const data = await fetch('http://colormind.io/api/', {
+    method: 'POST',
+    body: JSON.stringify({model: "default"}),
+  })
+  .then(async (data) => await data.json());
+  const parent = document.getElementById('sectionsParent');
+
+  for (let i = 0; i < parent.children.length; i++) {
+    parent.children[i].style.backgroundColor = `rgb(${data.result[i][0]}, ${data.result[i][1]}, ${data.result[i][2]})`;
+  }
+  return data;
 };
