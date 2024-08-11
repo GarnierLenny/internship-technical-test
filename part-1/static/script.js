f7daf60858d90b28cffa7de7340f09db35948e84
GarnierLenny <61317098+GarnierLenny@users.noreply.github.com>
2024-08-11 16:01:07 +0400

    FEAT - added hex values of colors

--- part-1/static/script.js
+++ part-1/static/script.js
@@ -1,3 +1,12 @@
+const componentToHex = (c) => {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+const rgbToHex = (r, g, b) => {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
 async function getNewPalette() {
   const data = await fetch('http://colormind.io/api/', {
     method: 'POST',
@@ -9,7 +18,12 @@ async function getNewPalette() {
   for (let i = 0; i < parent.children.length; i++) {
     const currentColor = `rgb(${data.result[i][0]}, ${data.result[i][1]}, ${data.result[i][2]})`;
     parent.children[i].style.backgroundColor = currentColor;
-    document.getElementById(`text${i + 1}`).textContent = currentColor;
+    document.getElementById(`rgb${i + 1}`).textContent = currentColor;
+    document.getElementById(`hex${i + 1}`).textContent = rgbToHex(
+      data.result[i][0],
+      data.result[i][1],
+      data.result[i][2]
+    );
   }
   return data;
 };
