97c13160a88a73e11ebcc586d17596654379a068
GarnierLenny <61317098+GarnierLenny@users.noreply.github.com>
2024-08-11 23:11:03 +0400

    FEAT - added base sections

--- part-2/src/App.tsx
+++ part-2/src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import './App.css';
 
 function App() {
+  const baseColors: string[] = [
+    '#ff0000',
+    '#ffa500',
+    '#ffff00',
+    '#008000',
+    '#0000ff',
+  ];
+
   return (
-    <div className="App">
-      <p>React app</p>
+    <div style={{display: 'flex'}}>
+      {baseColors.map((section, index) => (
+        <div style={{backgroundColor: section, flex: 1, height: '100vh'}} key={index}>
+          <p></p>
+        </div>
+      ))}
     </div>
   );
 }
