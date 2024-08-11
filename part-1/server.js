2e2f8b4c306b4861d70af50a0a597a34c0f2c818
GarnierLenny <61317098+GarnierLenny@users.noreply.github.com>
2024-08-11 14:07:21 +0400

    FEAT - server now loads static files

--- part-1/server.js
+++ part-1/server.js
@@ -6,6 +6,19 @@ const port = process.env.port | 8080;
 const host = 'localhost';
 
 const server = http.createServer((req, res) => {
+  if (req.url.includes('/static')) {
+    fs.readFile(path.join(__dirname, req.url), (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-type': 'text/html' });
+        res.end(`<html><h1>Could not load static file: ${err}</h1></html>`);
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'application/javascript' });
+      res.end(data);
+    });
+    return;
+  }
+
   fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
     if (err) {
       res.writeHead(500, { 'Content-type': 'text/html' });
@@ -15,6 +28,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(data);
   });
+  return;
 });
 
 server.listen(port, host, () => {
