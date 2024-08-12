4020c395bd2b3178c41c2c60aa90b1a9194b2e43
GarnierLenny <61317098+GarnierLenny@users.noreply.github.com>
2024-08-12 14:51:24 +0400

    FEAT - part-3 can now generate palette

--- part-3/src/pages/index.tsx
+++ part-3/src/pages/index.tsx
@@ -16,12 +16,37 @@ export default function Home() {
   const defaultModel: string = 'default';
   const [locked, setLocked] = useState<boolean[]>(Array(5).fill(false));
 
+  const getNewPalette = async () => {
+    const data = await fetch('http://colormind.io/api/', {
+      method: 'POST',
+      body: JSON.stringify({
+        model: 'default',
+        input: colors.map((color, index) => locked[index] ? color.split(',').map(value => parseInt(value)) : "N"),
+      }),
+    })
+    .then(async (data) => await data.json())
+    .catch(err => err);
+
+    for (let i = 0; i < 5; i++) {
+      setColors((oldColors => {
+        const currentColor = `${data.result[i][0]}, ${data.result[i][1]}, ${data.result[i][2]}`;
+        const newColors: string[] = [...oldColors];
+
+        newColors[i] = currentColor;
+        return newColors;
+      }));
+    }
+    return data;
+  };
+
+
   return (
     <main
       className={`flex min-h-screen flex-col ${inter.className}`}
     >
       <Toaster />
       <Sections lock={{locked, setLocked}} colors={colors} />
+      <button className="absolute bottom-0 w-screen py-4 bg-blue-700 hover:bg-blue-900" onClick={getNewPalette}>Generate new palette</button>
     </main>
   );
 }
